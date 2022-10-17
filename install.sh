d39848fb4760f589e1638d543683640a57e91ad0
Nandi Richard <94351277+nandirchrd@users.noreply.github.com>
2022-10-17 23:14:09 +0700

    fixed bugs

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 rm -rf $PREFIX/etc/apt/sources.list.d
 echo deb https://packages.termux.dev/termux-main-21/ 
 stable main > $PREFIX/etc/apt/sources.list
-apt update && apt upgrade -y
+apt update 
+apt upgrade -y
